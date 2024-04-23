56283f75ecbcedf45a652e7fba623dd791a165bd
AriReboucas <arireboucasdev@gmail.com>
2024-04-23 17:45:10 -0300

    feat: #11 configurando toastify errorMessage e styles(css)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FiSearch } from "react-icons/fi";
 import { IMaskInput } from "react-imask";
+import { toast } from "react-toastify";
 import "./styles.css";
 import api from "./services/api";
 
 function App() {
   const [input, setInput] = useState("");
   const [cep, setCep] = useState({});
+  const [errorMessage, setErrorMessage] = useState("");
+  const [showError, setShowError] = useState(false);
 
   function handleKeyPress(event) {
     if (event.key === "Enter") {
@@ -14,18 +17,28 @@ function App() {
     }
   }
 
+  useEffect(() => {
+    if (showError) {
+      setTimeout(() => {
+        setShowError(false);
+      }, [5000]);
+    }
+  }, [showError]);
+
   async function handleSearch() {
     if (input === "") {
-      alert("Preencha algum CEP!");
+      setErrorMessage("Preencha algum CEP!");
+      setShowError(true);
       return;
     }
 
-    try {
-      const response = await api.get(`${input}/json`);
-      setCep(response.data);
+    const response = await api.get(`${input}/json`);
+    if (response.data.erro) {
+      setErrorMessage("Ops... erro ao buscar o CEP digitado!");
+      setShowError(true);
       setInput("");
-    } catch {
-      alert("Ops... erro ao buscar o CEP digitado!");
+    } else {
+      setCep(response.data);
       setInput("");
     }
   }
@@ -47,6 +60,8 @@ function App() {
         <button className="buttonSearch" onClick={handleSearch}>
           <FiSearch size={25} color="#FFF" />
         </button>
+
+        {showError && <div className="error-message">{errorMessage}</div>}
       </div>
 
       {Object.keys(cep).length > 0 && (
