c6d91ff3f5ac75c1ce2fc1eb8bc0f4e1f5c76078
AriReboucas <arireboucasdev@gmail.com>
2024-04-23 21:59:30 -0300

    feat: #7 improve GA

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import ReactGA from "react-ga4";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactGA.initialize("G-NCEGMJ9KLP");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
